79dfe666f5b1a166cebcb0c06b0ce37d4839bde9
Christopher Klemenz <26851310+klemchri@users.noreply.github.com>
2022-11-15 11:46:27 +0000

    commit

---
+++ Rechteck.java
@@ -0,0 +1,72 @@
+import java.lang.Math;
+
+public class Rechteck {
+
+    private double laenge;
+    private double breite;
+
+    public Rechteck(double laenge, double breite) {
+        this.laenge = laenge;
+        this.breite = breite;
+    }
+
+    public Rechteck(double laenge) {
+        this(laenge, laenge);
+    }
+
+    public Rechteck() {
+        this(100, 200);
+    }
+
+    public double getLaenge() {
+        return laenge;
+    }
+
+    public void setLaenge(double laenge) {
+        if (laenge < 0) {
+            this.laenge = laenge;
+        }else{
+            throw new IllegalArgumentException("Länge darf nicht negativ sein!");
+        }
+    }
+
+    public double getBreite() {
+        return breite;
+    }
+
+    public void setBreite(double breite) {
+        if (breite < 0) {
+            this.breite = breite;
+        }else{
+            throw new IllegalArgumentException("Breite darf nicht negativ sein!");
+        }
+
+    }
+
+    public double umfang() {
+        return 2 * (laenge + breite);
+    }
+
+    public double flaeche() {
+        return laenge * breite;
+    }
+
+    @Override
+    public String toString() {
+        if (laenge == breite) {
+            return "Quadrat mit Seitenlänge = " + laenge + ", Fläche = " + this.flaeche() + " und  Umfang = " + this.umfang();
+        } else {
+            return "Rechteck mit Länge = " + laenge + ", Breite = " + breite + ", Fläche = " + this.flaeche() + " und  Umfang = " + this.umfang();
+        }
+    }
+
+    public Rechteck toQuadratFlaeche() {
+        double flaeche = Math.sqrt(this.flaeche());
+        return new Rechteck(flaeche);
+    }
+
+    public String tell_me_more() {
+        System.out.println("This is a Rechteck");
+    }
+
+}
--- start.java
+++ start.java
@@ -3,6 +3,13 @@ public class start {
     public static void main(String[] args) {
         Gruss g1 = new Gruss();
         Gruss g2 = new Gruss("Hans");
+
+        Rechteck r1 = new Rechteck(3, 12);
+        Rechteck quadrat = r1.toQuadratFlaeche();
+        
+        System.out.println(r1);
+        System.out.println(quadrat);
+        
     }
 
 }
