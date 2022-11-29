059886407eb7f5b79e05d1a807439774dd4b4417
Christopher Klemenz <26851310+klemchri@users.noreply.github.com>
2022-11-29 08:49:41 +0000

    Main in Test aufteilen, Fraction Klasse hinzufuegen

--- start.java
+++ start.java
@@ -1,34 +1,51 @@
 public class start {
 
     public static void main(String[] args) {
-        Gruss g1 = new Gruss();
-        Gruss g2 = new Gruss("Hans");
+        testFraction();
+        // testBruch();
+        // testRechteck();
+        // testGruss();
 
-        System.out.println("Schulrechecke:");
         
-        Rechteck schule1 = new Rechteck();
-        Rechteck schule2 = new Rechteck(100);
-        System.out.println(schule1);
-        System.out.println(schule2);
+    }
 
-
-        System.out.println("Eigene Versuche:");
-        Rechteck r1 = new Rechteck(3, 12);
-        Rechteck quadrat = r1.toQuadratFlaeche();
+    public static void testFraction(){
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(1, 3);
         
-        System.out.println(r1);
-        System.out.println(quadrat);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f1.multiply(f2));
+        System.out.println(f1.divideBy(f2));
+    }
 
-        r1.tell_me_more();
-
+    public static void testBruch(){
         System.out.println("Bruch:");
         Bruch b1 = new Bruch(1, 2);
         Bruch b2 = new Bruch(3, 4);
         System.out.println(b1);
         System.out.println(b2);
         System.out.println(b1.toDoubleString());
-        
-        
     }
 
+    public static void testRechteck(){
+        System.out.println("Schulrechecke:");
+        Rechteck schule1 = new Rechteck();
+        Rechteck schule2 = new Rechteck(100);
+        System.out.println(schule1);
+        System.out.println(schule2);
+        System.out.println("Eigene Versuche:");
+        Rechteck r1 = new Rechteck(3, 12);
+        Rechteck quadrat = r1.toQuadratFlaeche();
+        System.out.println(r1);
+        System.out.println(quadrat);
+        r1.tell_me_more();
+    }
+
+    public static void testGruss(){
+        Gruss g1 = new Gruss();
+        Gruss g2 = new Gruss("Hans");
+        System.out.println(g1);
+        System.out.println(g2);
+    }
 }
